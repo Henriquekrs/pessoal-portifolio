8d2ff08e6e99b5d8a72b3891c8ef883acafcfcdf
Gustavo Henrique R Duarte <ghrduarte@hotmail.com>
2024-12-12 01:45:14 -0300

    Preparação para aplicação de rolagem suave

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import LocomotiveScroll from 'locomotive-scroll';
 import { RepoList } from '../components/RepoList';
-
-import 'locomotive-scroll/src/locomotive-scroll.scss';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
+import 'locomotive-scroll/dist/locomotive-scroll.css'; // Importa o CSS da biblioteca
+
 export function Home() {
+  const scrollRef = useRef(null); // Referência para o contêiner de rolagem
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current, // Define o elemento monitorado
+      smooth: true, // Ativa a rolagem suave
+    });
+
+    // Cleanup para evitar problemas ao desmontar o componente
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
+
   return (
-    <div>
+    <div data-scroll-container ref={scrollRef}>
       <Header />
       <RepoList />
       <Footer />
